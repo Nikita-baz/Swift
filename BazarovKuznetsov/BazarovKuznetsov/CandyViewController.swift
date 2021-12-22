ed043db305a7703a97b446995bcf8461feee45e0
Nikita Bazarov <81754289+Nikita-baz@users.noreply.github.com>
2021-12-22 20:43:01 +0300

    Лабораторные работы

---
+++ BazarovKuznetsov/BazarovKuznetsov/CandyViewController.swift
@@ -0,0 +1,33 @@
+//
+//  CandyViewController.swift
+//  Bazarov*Kuznetsov
+//
+//  Created by Student on 01.12.2021.
+//
+
+import UIKit
+
+class CandyViewController: UIViewController {
+    @IBOutlet weak var candyPicture: UIImageView!
+
+    override func viewWillAppear(_ animated: Bool) {
+        UIView.animate (withDuration: 1.0, delay: 1.0, options: [.repeat, .autoreverse], animations: {
+            self.candyPicture.frame.size.width = 105
+            self.candyPicture.frame.size.width = 54
+        }, completion: nil)
+    }
+        }
+
+        // Do any additional setup after loading the view.
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
